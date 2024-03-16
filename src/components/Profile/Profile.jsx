901f66301380e6f1af45848d97c2aeff77a32a87
Rick <46992687+rick-77-2006@users.noreply.github.com>
2024-03-16 22:04:57 +0200

    update-2024-24

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,16 +9,16 @@ const Profile = ({ name, tag, location, image, stats }) => {
         <p className={css.userFont}>@{tag}</p>
         <p className={css.userFont}>{location}</p>
       </div>
-      <ul className={css.statsBlock}>
-        <li className={css.statsBlockItem}>
+      <ul className={css.statsList}>
+        <li className={css.statsListItem}>
           <span className={css.listTitle}>Followers</span>
           <span className={css.listAmount}>{stats.followers}</span>
         </li>
-        <li className={css.statsBlockItem}>
+        <li className={css.statsListItem}>
           <span className={css.listTitle}>Views</span>
           <span className={css.listAmount}>{stats.views}</span>
         </li>
-        <li className={css.statsBlockItem}>
+        <li className={css.statsListItem}>
           <span className={css.listTitle}>Likes</span>{" "}
           <span className={css.listAmount}>{stats.likes}</span>
         </li>
